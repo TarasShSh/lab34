951c68d53efce9eee4c86069a8a190eff5401652
Taras Shevchenko <taras.shevchenko.itis.2020@lpnu.ua>
2020-12-06 19:09:22 +0200

    first

---
+++ lab34/lab34.cpp
@@ -0,0 +1,36 @@
+﻿// lab34.cpp
+// Шевченко Тарас
+// Лабораторна робота No 3.4
+// Розгалуження, задане графіком функції
+// Варіант 23
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double x;
+	double y;
+	double R;
+
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	// розгалуження в повній формі
+	if (x < 0 && y > 0)
+		if ((x >= sqrt(2 * y * R - pow(y, 2)) - R) && (x >= -R) && (y <= R))
+			cout << "yes" << endl;
+			else
+			cout << "no" << endl;
+	else if (x > 0 && y < 0)
+		if ((x <= -sqrt(-2 * y * R - pow(y, 2)) + R) && (y >= -R) && (x <= R))
+			cout << "yes" << endl;
+			else
+			cout << "no" << endl;
+	else if (( x >= -R && x <= R) && (y > -R && y < R))
+			cout << "yes" << endl;
+			else
+			cout << "no" << endl;
+}
